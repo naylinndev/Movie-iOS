f3e6cf7735f6f1c67cf3c7a5e422cf98c7ed96ba
Nay Linn <naylinnphyo00@gmail.com>
2022-07-14 22:09:11 +0630

    Initial Commit

---
+++ movie/movieApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieApp.swift
+//  movie
+//
+//  Created by Nay Linn on 7/14/22.
+//
+
+import SwiftUI
+
+@main
+struct movieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
